5c2440ad476bba47730216238a85806d35157e66
Aulia Samudra Jimananda <auliasamudra99@gmail.com>
2023-07-11 21:36:27 +0700

    Storage Context menu

--- src/pages/StoragePage.jsx
+++ src/pages/StoragePage.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { HiFolder, HiOutlineDownload, HiOutlineUpload, HiPlusSm, HiChevronDown, HiSearch, HiOutlineDotsVertical, HiDatabase } from 'react-icons/hi';
 
-const FolderCard = () => {
+const RightClickMenu = ({ x, y, options, onClose }) => {
   return (
-    <div className="w-[223px] h-[66px] border rounded-lg m-2 flex items-center space-x-4 bg-white shadow-lg">
+    <div style={{position: 'absolute', top: y, left: x, background: 'white', boxShadow: '0px 0px 2px rgba(0,0,0,0.15)', borderRadius: '5px', zIndex: 10}}>
+      {options.map(option => 
+        <button 
+          key={option} 
+          onClick={onClose} 
+          style={{display: 'block', padding: '10px', borderBottom: '1px solid #e5e5e5', width: '150px', textAlign: 'left'}}
+        >
+          {option}
+        </button>
+      )}
+    </div>
+  );
+};
+
+const FolderCard = () => {
+  const [contextMenu, setContextMenu] = useState({ visible: false, x: 0, y: 0 });
+
+  const handleContextMenu = (event) => {
+    event.preventDefault();
+    setContextMenu({ visible: true, x: event.clientX, y: event.clientY });
+  };
+
+  const handleCloseContextMenu = () => {
+    setContextMenu({ visible: false, x: 0, y: 0 });
+  };
+
+  return (
+    <div 
+      className="w-[223px] h-[66px] border rounded-lg m-2 flex items-center space-x-4 bg-white shadow-lg" 
+      onContextMenu={handleContextMenu} 
+      onClick={handleCloseContextMenu}
+    >
       <div className="pl-5">
         <HiFolder className="h-6 w-6 text-black" />
       </div>
@@ -11,13 +42,36 @@ const FolderCard = () => {
         <h2 className="text-xs font-[15]">Folder Name</h2>
         {/* Add other details here */}
       </div>
+      {contextMenu.visible && 
+        <RightClickMenu 
+          x={contextMenu.x} 
+          y={contextMenu.y} 
+          options={['Open', 'Delete', 'Rename']} 
+          onClose={handleCloseContextMenu}
+        />
+      }
     </div>
   );
 };
 
 const FileCard = () => {
+  const [contextMenu, setContextMenu] = React.useState({ visible: false, x: 0, y: 0 });
+
+  const handleContextMenu = (event) => {
+    event.preventDefault();
+    setContextMenu({ visible: true, x: event.clientX, y: event.clientY });
+  };
+
+  const handleCloseContextMenu = () => {
+    setContextMenu({ visible: false, x: 0, y: 0 });
+  };
+
   return (
-    <div className="w-[223px] h-[234px] border rounded-lg m-2 flex-col space-y-4 bg-white shadow-lg p-0">
+    <div 
+      className="w-[223px] h-[234px] border rounded-lg m-2 flex-col space-y-4 bg-white shadow-lg p-0" 
+      onContextMenu={handleContextMenu}
+      onClick={handleCloseContextMenu}
+    >
       <img className="w-full h-[170px] object-cover rounded-t-lg"
            src='https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg'
            alt='Preview'/>
@@ -25,14 +79,35 @@ const FileCard = () => {
         <h2 className="font-[15]">File Name</h2>
         <HiOutlineDotsVertical />
       </div>
+      {contextMenu.visible && 
+        <RightClickMenu 
+          x={contextMenu.x} 
+          y={contextMenu.y} 
+          options={['Open', 'Delete', 'Rename', 'ngab']} 
+          onClose={handleCloseContextMenu}
+        />
+      }
     </div>
   );
 };
 
-
 const StoragePage = () => {
+  const [contextMenu, setContextMenu] = useState({ visible: false, x: 0, y: 0 });
+
+  const handleCloseContextMenu = () => {
+    setContextMenu({ visible: false, x: 0, y: 0 });
+  };
+  
   return (
     <>
+      {contextMenu.visible && 
+        <RightClickMenu 
+          x={contextMenu.x} 
+          y={contextMenu.y} 
+          options={['Open', 'Delete', 'Rename']} 
+          onClose={handleCloseContextMenu}
+        />
+      }
       <div className="flex gap-4 p-4 items-center bg-white shadow-md">
         <button className="px-5 py-2 bg-blue-500 text-white flex justify-center items-center rounded-md font-[14]">
           <HiPlusSm className="mr-2 w-5 h-auto"/> New <HiChevronDown className="ml-2 w-5 h-auto"/>
@@ -85,8 +160,6 @@ const StoragePage = () => {
           <FileCard />
         </div>
       </div>
-
-
     </>
   );
 };
